51d51e4cb76b9a4eb11d3d9e27745b24d045703d
Matt Malone <mattagape@me.com>
2024-09-26 18:57:52 -0500

    Set up continuous scrolling

--- src/components/EpubReader.js
+++ src/components/EpubReader.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactReader } from 'react-reader';
 
 export const EpubReader = ({ epubUrl }) => {
-  const [location, setLocation] = useState(0); // Initialize location to 0
+  const [location, setLocation] = useState(localStorage.getItem('epub-location') || 0); // Initialize location to 0
+
+  const onLocationChanged = (epubcfi) => {
+    setLocation(epubcfi);
+    localStorage.setItem('epub-location', epubcfi);
+  }
+
+  useEffect(() => {
+    const savedLocation = localStorage.getItem('epub-location');
+    if (savedLocation) {
+      setLocation(savedLocation);
+    }
+  }, []);
 
   return (
   <div style={{ height: '100vh' }}>
     <ReactReader
       url={epubUrl}                         // URL of the EPUB file
       location={location}                   // Current location in the book
-      locationChanged={(epubcfi) => setLocation(epubcfi)} // Update location on change
+      locationChanged={onLocationChanged} // Update location on change
       epubOptions={{
         allowScriptedContent: true,
         allowPopups: false,
