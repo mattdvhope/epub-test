7241d18d79af30333013f931b5bc4ae4585f23d5
Matt Malone <mattagape@me.com>
2024-09-26 18:19:07 -0500

    Set up continuous scrolling

--- src/components/EpubReader.js
+++ src/components/EpubReader.js
@@ -5,13 +5,20 @@ export const EpubReader = ({ epubUrl }) => {
   const [location, setLocation] = useState(0); // Initialize location to 0
 
   return (
-    <div style={{ height: '100vh' }}>
-      <ReactReader
-        url={epubUrl}                         // URL of the EPUB file
-        location={location}                   // Current location in the book
-        locationChanged={(epubcfi) => setLocation(epubcfi)} // Update location on change
-      />
-    </div>
+  <div style={{ height: '100vh' }}>
+    <ReactReader
+      url={epubUrl}                         // URL of the EPUB file
+      location={location}                   // Current location in the book
+      locationChanged={(epubcfi) => setLocation(epubcfi)} // Update location on change
+      epubOptions={{
+        allowScriptedContent: true,
+        allowPopups: false,
+        manager: 'continuous',
+        flow: "scrolled",
+        offset: 0
+      }}
+    />
+  </div>
   );
 };
 
