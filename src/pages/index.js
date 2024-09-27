a358b26c576e58f71bd187f75ca98cc6bb6fe453
Matt Malone <mattagape@me.com>
2024-09-27 09:01:01 -0500

    Change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import EpubReader from "../components/EpubReader";
 
-const IndexPage = () => {
+const IndexPage  = () => {
   return (
     <div>
       <EpubReader epubUrl="/Joes-Book-Generic.epub" />
