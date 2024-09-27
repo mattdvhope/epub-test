c31806324b3091a69ac2cff58d9128741645112d
Matt Malone <mattagape@me.com>
2024-09-26 21:24:03 -0500

    Updated

--- src/components/EpubReader.js
+++ src/components/EpubReader.js
@@ -5,29 +5,17 @@ import useLocalStorageState from 'use-local-storage-state';
 export const EpubReader = ({ epubUrl }) => {
   const [location, setLocation] = useLocalStorageState('epub-location', 0); // Initialize location to 0
 
-  const onLocationChanged = (epubcfi) => {
-    setLocation(epubcfi);
-  };
-
   return (
     <div style={{ height: '100vh' }}>
       <ReactReader
         url={epubUrl} // URL of the EPUB file
         location={location} // Current location in the book
-        locationChanged={onLocationChanged} // Update location on change
+        locationChanged={setLocation} // Update location on change
         epubOptions={{
           allowScriptedContent: true,
           allowPopups: false,
-          offset: 0
-        }}
-        renderIframe={(iframe) => {
-          return (
-            <iframe
-              title="Ebook Reader" // Unique title for accessibility
-              src={iframe.src}
-              sandbox="allow-scripts" // Adjust as necessary for security
-            />
-          );
+          manager: 'paginated', // Use paginated flow
+          flow: "default", // Ensure you're using the default flow
         }}
       />
     </div>
