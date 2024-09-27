85bae2a1c51b6d11625212854e140bf7fcbe45db
Matt Malone <mattagape@me.com>
2024-09-26 21:16:33 -0500

    Updated

--- src/components/EpubReader.js
+++ src/components/EpubReader.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ReactReader } from 'react-reader';
+import useLocalStorageState from 'use-local-storage-state';
 
 export const EpubReader = ({ epubUrl }) => {
-  const [location, setLocation] = useState(localStorage.getItem('epub-location') || 0); // Initialize location to 0
+  const [location, setLocation] = useLocalStorageState('epub-location', 0); // Initialize location to 0
 
   const onLocationChanged = (epubcfi) => {
     setLocation(epubcfi);
-    localStorage.setItem('epub-location', epubcfi);
-  }
-
-  useEffect(() => {
-    const savedLocation = localStorage.getItem('epub-location');
-    if (savedLocation) {
-      setLocation(savedLocation);
-    }
-  }, []);
+  };
 
   return (
-  <div style={{ height: '100vh' }}>
-    <ReactReader
-      url={epubUrl}                         // URL of the EPUB file
-      location={location}                   // Current location in the book
-      locationChanged={onLocationChanged} // Update location on change
-      epubOptions={{
-        allowScriptedContent: true,
-        allowPopups: false,
-        manager: 'continuous',
-        flow: "scrolled",
-        offset: 0
-      }}
-    />
-  </div>
+    <div style={{ height: '100vh' }}>
+      <ReactReader
+        url={epubUrl} // URL of the EPUB file
+        location={location} // Current location in the book
+        locationChanged={onLocationChanged} // Update location on change
+        epubOptions={{
+          allowScriptedContent: true,
+          allowPopups: false,
+          offset: 0
+        }}
+        renderIframe={(iframe) => {
+          return (
+            <iframe
+              title="Ebook Reader" // Unique title for accessibility
+              src={iframe.src}
+              sandbox="allow-scripts" // Adjust as necessary for security
+            />
+          );
+        }}
+      />
+    </div>
   );
 };
 
