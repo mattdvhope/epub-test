6f3327fa552cef4fb89ef81b68a7ef5ded90ecd7
Matt Malone <mattagape@me.com>
2024-09-30 09:50:38 -0500

    Changed epub doc

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import EpubReader from "../components/EpubReader";
 const IndexPage  = () => {
   return (
     <div>
-      <EpubReader epubUrl="/Joes-Edited.epub" />
+      <EpubReader epubUrl="/Tarzan.epub" />
     </div>
   );
 };
