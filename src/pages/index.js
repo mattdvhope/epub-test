094d10bbb8c74f5afc2c0ed990d094d722475337
Matt Malone <mattagape@me.com>
2024-09-30 09:20:41 -0500

    Changed epub doc

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import EpubReader from "../components/EpubReader";
 const IndexPage  = () => {
   return (
     <div>
-      <EpubReader epubUrl="/Joes-Book-Generic.epub" />
+      <EpubReader epubUrl="/Joes-Edited.epub" />
     </div>
   );
 };
